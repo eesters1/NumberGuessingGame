55e0b81a99757e9548d9d8ab64bbd5a7a747ee4f
eesters1 <45569423+eesters1@users.noreply.github.com>
2022-11-01 14:10:14 -0500

    Initial Commit

---
+++ src/NumberGuessingGame.java
@@ -0,0 +1,34 @@
+import java.util.Random;
+import java.util.Scanner;
+
+public class NumberGuessingGame {
+
+    public static void main(String[] args) {
+        Random rand = new Random();
+        int numberToGuess = rand.nextInt(1000);
+        int numberOfTries = 0;
+        Scanner input = new Scanner(System.in);
+        int guess;
+        boolean win = false;
+
+        while (win==false){
+            System.out.println("Guess a number between 1 and 1000: ");
+            guess = input.nextInt();
+            numberOfTries++;
+
+            if (guess==numberToGuess){
+                win = true;
+            }
+            else if (guess < numberToGuess){
+                System.out.println("Your guess is too low");
+            }
+            else if (guess > numberToGuess){
+                System.out.println("Your guess is too high");
+            }
+        }
+
+        System.out.println("You win!");
+        System.out.println("The number was " + numberToGuess);
+        System.out.println("It took you " + numberOfTries + " tries");
+    }
+}
